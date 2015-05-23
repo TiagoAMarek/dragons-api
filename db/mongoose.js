d49815fd8555fe57c879fd6df49f31bc5f77c214
William Bruno <rocha_bruno@hotmail.com>
2015-05-23 20:00:39 -0300

    Typo mongo.server

--- db/mongoose.js
+++ db/mongoose.js
@@ -4,11 +4,11 @@ var mongoose  = require('mongoose'),
 
 'use strict';
 function _connection(vars) {
-  var username = vars.MONGO_USERNAME || config.get('mongo.username'),
-      password = vars.MONGO_PASSWORD || config.get('mongo.password'),
-      server   = vars.MONGO_SERVER   || config.get('mongo.server'),
-      port     = vars.MONGO_PORT     || config.get('mongo.port'),
-      database = vars.MONGO_DATABASE || config.get('mongo.database'),
+  var username  = vars.MONGO_USERNAME || config.get('mongo.username'),
+      password  = vars.MONGO_PASSWORD || config.get('mongo.password'),
+      host      = vars.MONGO_HOST     || config.get('mongo.host'),
+      port      = vars.MONGO_PORT     || config.get('mongo.port'),
+      database  = vars.MONGO_DATABASE || config.get('mongo.database'),
 
       auth = username ? /* istanbul ignore next */ username + ':' + password + '@' : '';
 
