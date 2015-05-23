45f364488e82eae4d27dde652dac81ccaa02dc62
William Bruno <rocha_bruno@hotmail.com>
2015-05-23 19:52:23 -0300

    Add Heroku env vars

--- db/mongoose.js
+++ db/mongoose.js
@@ -3,19 +3,19 @@ var mongoose  = require('mongoose'),
     config    = require('config');
 
 'use strict';
-function _connection() {
-  var username  = config.get('mongo.username'),
-      password  = config.get('mongo.password'),
-      host      = config.get('mongo.host'),
-      port      = config.get('mongo.port'),
-      database  = config.get('mongo.database'),
+function _connection(vars) {
+  var username = vars.MONGO_USERNAME || config.get('mongo.username'),
+      password = vars.MONGO_PASSWORD || config.get('mongo.password'),
+      server   = vars.MONGO_SERVER   || config.get('mongo.server'),
+      port     = vars.MONGO_PORT     || config.get('mongo.port'),
+      database = vars.MONGO_DATABASE || config.get('mongo.database'),
 
       auth = username ? /* istanbul ignore next */ username + ':' + password + '@' : '';
 
   return 'mongodb://' + auth + host + ':' + port + '/' + database;
 }
 
-mongoose.connect(_connection());
+mongoose.connect(_connection(process.env));
 var db = mongoose.connection;
 /* istanbul ignore next */
 db.on('error', function(err) {
